83a2913eb98f10825a80f139bf29fba6d493d096
subeend <soorud05@kyonggi.ac.kr>
2024-10-01 23:22:36 +0900

    Commit 1

---
+++ kwh2.py
@@ -0,0 +1,11 @@
+
+num1 = int(input("첫 번째 정수를 입력하세요: "))
+num2 = int(input("두 번째 정수를 입력하세요: "))
+
+
+sum_result = num1 + num2
+diff_result = num1 - num2
+
+
+print(f"두 수의 합: {sum_result}")
+print(f"두 수의 차: {diff_result}")
